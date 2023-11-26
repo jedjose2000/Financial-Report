ddf47b7b2bec4567c97116df3771981dadd892c9
jedjose2000 <jedjose2000@gmail.com>
2023-11-26 19:05:13 +0800

    Financial Report pt 1

---
+++ app/App.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+// Your Code
+function getAllTransactions()
+{
+    $row = 1;
+    $readFile = readAllFiles((FILES_PATH));
+    if ($readFile !== NULL) {
+        foreach ($readFile as $value) {
+            if(!file_exists($value)) {
+                error_log('File ' . $value . ' does not exist');
+                continue; 
+            }
+            $openedFile = fopen($value, 'r');
+            fgetcsv($openedFile);
+            while (($data = fgetcsv($openedFile, 1000, ",")) !== FALSE) {
+                $row++;
+                $dataArray[] = [
+                    'date' => $data[0] ?? null,
+                    'check' => $data[1] ?? null,
+                    'transaction' => $data[2] ?? null,
+                    'amount' => $data[3] ?? null
+                ];
+            }
+            fclose($openedFile);
+        }
+    }
+    return $dataArray;
+}
+
+function readAllFiles($files){
+    $dir = new DirectoryIterator($files);
+    foreach ($dir as $file) {
+        if (!$file->isDot()) {
+            $filePath []=  FILES_PATH . $file->getFilename();
+        }
+    }
+    return $filePath;
+}
+
+function computeTransaction($transactions){
+    $totalIncome = 0;
+    $totalExpenses = 0;
+    foreach ($transactions as $key => $value){
+        $temp = str_replace([',','$'], '', $value['amount']); // Removing commas for numeric consistency
+        if($temp >= 0){
+            $totalIncome += $temp;
+        }else{
+            $totalExpenses -= $temp;
+        }
+    }
+    return $data = [
+        'totalIncome'=> $totalIncome,
+        'totalExpenses'=> $totalExpenses
+    ];
+}
+
---
+++ views/transactions.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Transactions</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            text-align: center;
+        }
+
+        table tr th,
+        table tr td {
+            padding: 5px;
+            border: 1px #eee solid;
+        }
+
+        tfoot tr th,
+        tfoot tr td {
+            font-size: 20px;
+        }
+
+        tfoot tr th {
+            text-align: right;
+        }
+    </style>
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>Check #</th>
+                <th>Description</th>
+                <th>Amount</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($transactionsArray as $key => $value): ?>
+                <tr>
+                    <td>
+                        <?= $value['date']; ?>
+                    </td>
+                    <td>
+                        <?= $value['check']; ?>
+                    </td>
+                    <td>
+                        <?= $value['transaction']; ?>
+                    </td>
+                    <td style="<?php  echo (str_replace('$', "", $value['amount'] ) < 0) ? 'color: red;' : 'color: green;'; ?>">
+                        <?= $value['amount']; ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th colspan="3">Total Income:</th>
+                <td><?php echo '$'. number_format($computedTransactions['totalIncome'],2,'.',',')  ?></td>
+            </tr>
+            <tr>
+                <th colspan="3">Total Expense:</th>
+                <td><?php echo '-$'. number_format($computedTransactions['totalExpenses'],2,'.',',') ?></td>
+            </tr>
+            <tr>
+                <th colspan="3">Net Total:</th>
+                <td><?php echo '$'. number_format($netIncome,2,'.',',') ?></td>
+            </tr>
+        </tfoot>
+    </table>
+</body>
+
+</html>
